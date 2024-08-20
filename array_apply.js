e8c34c2644c801b150c8395557d91b2b8cad9944
Al-Abir <sheikhabir196@gmail.com>
2024-08-20 22:22:38 +0600

    code

---
+++ array_apply.js
@@ -0,0 +1,21 @@
+function map(arr, fn) {
+    const newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArray.push(fn(arr[i], i));
+    }
+    return newArray;
+}
+
+// Example usage
+const arr = [1, 2, 3];
+const plusOne = function(n) { return n + 1; };
+console.log(map(arr, plusOne)); // Output: [2, 3, 4]
+
+
+var map = function(arr, fn) {
+    const transformedArr = [];
+    arr.forEach((element, index) => {
+      transformedArr[index] = fn(element,index);
+    });
+    return transformedArr;
+  };
